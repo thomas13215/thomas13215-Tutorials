fe6701f2f9798e49ad5a1fc07bb452131300a4a1
thomas13215 <48337338+thomas13215@users.noreply.github.com>
2020-07-14 13:29:45 -0500

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,18 +1,12 @@
-local usernames = {
+local usernames = { --put blacklisted users here
     "name1",
     "name2",
 }
 --Put usernames above
-
 game.Players.PlayerAdded:Connect(function(plr) -- Function gets the player
-	for i, v in pairs(usernames) do --Checking table
-		if v == plr.Name then --If the player is whitelisted
-			--insert function here
-        else
-            plr:Kick("Blacklisted") --kicks the player
-		end
+	if table.find(usernames, plr.Name) then --checking the table to see if player name matches
+		plr:Kick("blacklisted") --kicks the person
 	end
 end)
 
 --Made by Fortress Master
-
